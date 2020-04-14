540b46ffdccc956d28377a8baa3e69e925b4ef5d
RidhwaanDev <ridhwaan.any@gmail.com>
2020-04-14 16:38:43 -0400

    do some more cleanup

--- appleping.c
+++ appleping.c
@@ -1,8 +1,5 @@
-#include <sys/cdefs.h>
-
-#include <sys/param.h>		
+#include <sys/param.h>
 #include <sys/socket.h>
-#include <sys/sysctl.h>
 #include <sys/time.h>
 #include <sys/uio.h>
 
@@ -10,52 +7,28 @@
 #include <netinet/in_systm.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-#include <netinet/ip_var.h>
 #include <arpa/inet.h>
-#include <net/if.h>
 
-#ifdef IPSEC
-#include <netinet6/ipsec.h>
-#endif /*IPSEC*/
 
-#include <ctype.h>
 #include <err.h>
 #include <errno.h>
-#include <math.h>
 #include <netdb.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
-#include <ifaddrs.h>
 
-#define	INADDR_LEN	((int)sizeof(in_addr_t))
 #define	TIMEVAL_LEN	((int)sizeof(struct tv32))
-#define	MASK_LEN	(ICMP_MASKLEN - ICMP_MINLEN)
-#define	TS_LEN		(ICMP_TSLEN - ICMP_MINLEN)
 #define	DEFDATALEN	56		/* default data length */
-#define	FLOOD_BACKOFF	20000		/* usecs to back off if F_FLOOD mode */
-					/* runs out of buffer space */
-#define	MAXIPLEN	(sizeof(struct ip) + MAX_IPOPTLEN)
-#define	MAXICMPLEN	(ICMP_ADVLENMIN + MAX_IPOPTLEN)
 #define	MAXWAIT		10000		/* max ms to wait for response */
-#define	MAXALARM	(60 * 60)	/* max seconds for alarm timeout */
-#define	MAXTOS		255
 
 #define IP_NO_IFT_CELLULAR  6969 /* for internal use only */
-#define IP_NO_IFT_PDP       IP_NO_IFT_CELLULAR /* deprecated */
-#define SO_TRAFFIC_CLASS        0x1086      /* Traffic class (int)*/
-#define SO_RECV_TRAFFIC_CLASS   0x1087      /* Receive traffic class (bool)*/
-#define SO_TC_CTL 0x1088      /* Receive traffic class (bool)*/
-#define SO_RECV_ANYIF 0x1089      /* Receive traffic class (bool)*/
 
 
 #define	A(bit)		rcvd_tbl[(bit)>>3]	/* identify byte in array */
 #define	B(bit)		(1 << ((bit) & 0x07))	/* identify bit in byte */
 #define	SET(bit)	(A(bit) |= B(bit))
-#define	CLR(bit)	(A(bit) &= (~B(bit)))
 #define	TST(bit)	(A(bit) & B(bit))
 
 struct tv32 {
@@ -66,31 +39,11 @@ struct tv32 {
 /* various options */
 int options;
 #define	F_FLOOD		0x0001
-#define	F_INTERVAL	0x0002
-#define	F_NUMERIC	0x0004
-#define	F_PINGFILLED	0x0008
 #define	F_QUIET		0x0010
-#define	F_RROUTE	0x0020
-#define	F_SO_DEBUG	0x0040
-#define	F_SO_DONTROUTE	0x0080
 #define	F_VERBOSE	0x0100
-#define	F_QUIET2	0x0200
-#define	F_NOLOOP	0x0400
-#define	F_MTTL		0x0800
-#define	F_MIF		0x1000
 #define	F_AUDIBLE	0x2000
 #ifdef IPSEC
-#ifdef IPSEC_POLICY_IPSEC
-#define F_POLICY	0x4000
-#endif /*IPSEC_POLICY_IPSEC*/
 #endif /*IPSEC*/
-#define	F_TTL		0x8000
-#define	F_MISSED	0x10000
-#define	F_ONCE		0x20000
-#define	F_HDRINCL	0x40000
-#define	F_MASK		0x80000
-#define	F_TIME		0x100000
-#define	F_SWEEP		0x200000
 #define	F_WAITTIME	0x400000
 
 /*
@@ -104,25 +57,17 @@ char rcvd_tbl[MAX_DUP_CHK / 8];
 
 struct sockaddr_in whereto;	/* who to ping */
 int datalen = DEFDATALEN;
-int maxpayload;
 int s;				/* socket file descriptor */
 u_char outpackhdr[IP_MAXPACKET], *outpack;
+
 char BBELL = '\a';		/* characters written for MISSED and AUDIBLE */
 char BSPACE = '\b';		/* characters written for flood */
-char DOT = '.';
 char *hostname;
-char *shostname;
 int ident;			/* process id to identify our packets */
-int uid;			/* cached uid for micro-optimization */
 u_char icmp_type = ICMP_ECHO;
 u_char icmp_type_rsp = ICMP_ECHOREPLY;
 int phdr_len = 0;
 int send_len;
-char *boundif;
-unsigned int ifscope;
-int nocell;
-int how_traffic_class = 0;
-int traffic_class = SO_TC_CTL;	/* use control class, by default */
 int no_dup = 0;
 
 /* counters */
@@ -131,12 +76,7 @@ long npackets;			/* max packets to transmit */
 long nreceived;			/* # of packets we got back */
 long nrepeats;			/* number of duplicates */
 long ntransmitted;		/* sequence # for outbound packets = #sent */
-long snpackets;			/* max packets to transmit in one sweep */
-long snreceived;		/* # of packets we got back in this sweep */
 long sntransmitted;		/* # of packets we sent in this sweep */
-int sweepmax;			/* max value of payload in sweep */
-int sweepmin = 0;		/* start value of payload in sweep */
-int sweepincr = 1;		/* payload increment in sweep */
 int interval = 1000;		/* interval between packets, ms */
 int waittime = MAXWAIT;		/* timeout for each packet */
 long nrcvtimeout = 0;		/* # of packets we got back after waittime */
@@ -148,90 +88,72 @@ double tmax = 0.0;		/* maximum round trip time */
 double tsum = 0.0;		/* sum of all times, for doing average */
 double tsumsq = 0.0;		/* sum of all times squared, for std. dev. */
 
-volatile sig_atomic_t finish_up;  /* nonzero if we've been told to finish up */
 
 static u_short in_cksum(u_short *, int);
-static void finish(void) __dead2;
 static void pinger(void);
 static void pr_pack(char *, int, struct sockaddr_in *, struct timeval *, int);
 static void tvsub(struct timeval *, const struct timeval *);
 
 int main(int argc, char *const *argv)
 {
-	struct sockaddr_in from, sock_in;
-	struct in_addr ifaddr;
+	struct sockaddr_in from;
 	struct timeval last, intvl;
 	struct iovec iov;
-	struct ip *ip;
 	struct msghdr msg;
-	struct sigaction si_sa;
-	size_t sz;
-	u_char *datap, packet[IP_MAXPACKET] __attribute__((aligned(4)));
-	char *ep, *source, *target, *payload;
+
+	// IP packet buffer
+	u_char packet[IP_MAXPACKET];
+
+	char *target;
+
 	struct hostent *hp;
 	struct sockaddr_in *to;
-	double t;
-	u_long alarmtimeout, ultmp;
-	int almost_done, ch, df, hold, i, icmp_len, mib[4], preload, sockerrno,
-	    tos, ttl;
-	char ctrl[CMSG_SPACE(sizeof(struct timeval)) + CMSG_SPACE(sizeof(int))];
-	char hnamebuf[MAXHOSTNAMELEN], snamebuf[MAXHOSTNAMELEN];
-	unsigned char loop, mttl;
-	payload = source = NULL;
 
-	if (getuid())
-		s = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
-	else
-		s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	sockerrno = errno;
+	int i, icmp_len;
 
-	if (setuid(getuid()) != 0)
-		err(EX_NOPERM, "setuid() failed");
-	uid = getuid();
+	char hnamebuf[MAXHOSTNAMELEN];
 
-	alarmtimeout = df = preload = tos = 0;
+	// create socket
+	if (getuid()) {
+        s = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+        printf("DGRAM");
+    } else {
+        s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+        printf("RAW");
+    }
 
 	outpack = outpackhdr + sizeof(struct ip);
 
 	target = argv[optind];
 
 	icmp_len = sizeof(struct ip) + ICMP_MINLEN + phdr_len;
-	maxpayload = IP_MAXPACKET - icmp_len;
 	send_len = icmp_len + datalen;
-	datap = &outpack[ICMP_MINLEN + phdr_len + TIMEVAL_LEN];
-
 	bzero(&whereto, sizeof(whereto));
 	to = &whereto;
 	to->sin_family = AF_INET;
 	to->sin_len = sizeof *to;
+
+	// hostname
 	if (inet_aton(target, &to->sin_addr) != 0) {
 		hostname = target;
 	} else {
 		hp = gethostbyname2(target, AF_INET);
 		if (!hp)
-			errx(EX_NOHOST, "cannot resolve %s: %s",
-			    target, hstrerror(h_errno));
+			errx(EX_NOHOST, "cannot resolve %s: %s",target, hstrerror(h_errno));
 		memcpy(&to->sin_addr, hp->h_addr_list[0], sizeof to->sin_addr);
-		(void)strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
+		strncpy(hnamebuf, hp->h_name, sizeof(hnamebuf) - 1);
 		hnamebuf[sizeof(hnamebuf) - 1] = '\0';
 		hostname = hnamebuf;
-	}
+        printf("hostname is %s\n", hostname);
+    }
 
-	bzero(&msg, sizeof(msg));
+	memset(&msg, 0, sizeof(msg));
 	msg.msg_name = (caddr_t)&from;
 	msg.msg_iov = &iov;
 	msg.msg_iovlen = 1;
 	iov.iov_base = packet;
 	iov.iov_len = IP_MAXPACKET;
 
-	if (preload == 0)
-		pinger();		/* send the first ping */
-	else {
-		if (npackets != 0 && preload > npackets)
-			preload = npackets;
-		while (preload--)	/* fire off them quickies */
-			pinger();
-	}
 	gettimeofday(&last, NULL);
 
 	if (options & F_FLOOD) {
@@ -242,15 +164,14 @@ int main(int argc, char *const *argv)
 		intvl.tv_usec = interval % 1000 * 1000;
 	}
 
-	almost_done = 0;
-	while (!finish_up) {
+	// PING LOOP starts here
+	while (1) {
+	    // printf("looping\n");
 		struct timeval now, timeout;
 		fd_set rfds;
 		int cc, n;
 		int tc = -1;
 
-		if ((unsigned)s >= FD_SETSIZE)
-			errx(EX_OSERR, "descriptor too large");
 		FD_ZERO(&rfds);
 		FD_SET(s, &rfds);
 		gettimeofday(&now, NULL);
@@ -266,16 +187,13 @@ int main(int argc, char *const *argv)
 		}
 		if (timeout.tv_sec < 0)
 			timeout.tv_sec = timeout.tv_usec = 0;
+		//wait until file descriptor is ready
 		n = select(s + 1, &rfds, NULL, NULL, &timeout);
 		if (n < 0)
 			continue;	/* Must be EINTR. */
+
 		if (n == 1) {
 			struct timeval *tv = NULL;
-#ifdef SO_TIMESTAMP
-			struct cmsghdr *cmsg = (struct cmsghdr *)&ctrl;
-
-			msg.msg_controllen = sizeof(ctrl);
-#endif
 			msg.msg_namelen = sizeof(from);
 			if ((cc = recvmsg(s, &msg, 0)) < 0) {
 				if (errno == EINTR)
@@ -283,54 +201,21 @@ int main(int argc, char *const *argv)
 				warn("recvmsg");
 				continue;
 			}
-			for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
-				if (cmsg->cmsg_level == SOL_SOCKET &&
-					cmsg->cmsg_type == SO_TRAFFIC_CLASS &&
-					cmsg->cmsg_len == CMSG_LEN(sizeof(int))) {
-					/* Copy to avoid alignment problems: */
-					memcpy(&tc, CMSG_DATA(cmsg), sizeof(tc));
-				}
-			}
 			if (tv == NULL) {
-				(void)gettimeofday(&now, NULL);
+				gettimeofday(&now, NULL);
 				tv = &now;
 			}
-			pr_pack((char *)packet, cc, &from, tv, tc);
-			if ((options & F_ONCE && nreceived) ||
-			    (npackets && nreceived >= npackets))
-				break;
+			  pr_pack((char *)packet, cc, &from, tv, tc);
 		}
+
 		if (n == 0 || options & F_FLOOD) {
-			if (sweepmax && sntransmitted == snpackets) {
-				for (i = 0; i < sweepincr ; ++i)
-					*datap++ = i;
-				datalen += sweepincr;
-				if (datalen > sweepmax)
-					break;
-				send_len = icmp_len + datalen;
-				sntransmitted = 0;
-			}
-			if (!npackets || ntransmitted < npackets)
-				pinger();
-			else {
-				if (almost_done)
-					break;
-				almost_done = 1;
-				intvl.tv_usec = 0;
-				if (nreceived) {
-					intvl.tv_sec = 2 * tmax / 1000;
-					if (!intvl.tv_sec)
-						intvl.tv_sec = 1;
-				} else {
-					intvl.tv_sec = waittime / 1000;
-					intvl.tv_usec = waittime % 1000 * 1000;
-				}
-			}
+			if (!npackets || ntransmitted < npackets) {
+                pinger();
+            }
+
 			gettimeofday(&last, NULL);
 			if (ntransmitted - nreceived - 1 > nmissedmax) {
 				nmissedmax = ntransmitted - nreceived - 1;
-				if (options & F_MISSED)
-					(void)write(STDOUT_FILENO, &BBELL, 1);
 				if (!(options & F_QUIET))
 					printf("Request timeout for icmp_seq %ld\n", ntransmitted - 2);
 			}
@@ -339,23 +224,9 @@ int main(int argc, char *const *argv)
 	exit(0);	/* Make the compiler happy */
 }
 
-
-/*
- * pinger --
- *	Compose and transmit an ICMP ECHO REQUEST packet.  The IP packet
- * will be added on by the kernel.  The ID field is our UNIX process ID,
- * and the sequence number is an ascending integer.  The first TIMEVAL_LEN
- * bytes of the data portion are used to hold a UNIX "timeval" struct in
- * host byte-order, to compute the round-trip time.
- */
-static void
-pinger(void)
-{
-	struct timeval now;
-	struct tv32 tv32;
-	struct ip *ip;
+static void pinger(){
 	struct icmp *icp;
-	int cc, i;
+	int cc;
 	u_char *packet;
 
 	packet = outpack;
@@ -363,89 +234,31 @@ pinger(void)
 	icp->icmp_type = icmp_type;
 	icp->icmp_code = 0;
 	icp->icmp_cksum = 0;
+	// convert to network byte order
 	icp->icmp_seq = htons(ntransmitted);
 	icp->icmp_id = ident;			/* ID */
 
-	CLR(ntransmitted % mx_dup_ck);
 
 	cc = ICMP_MINLEN + phdr_len + datalen;
 
 	/* compute ICMP checksum here */
 	icp->icmp_cksum = in_cksum((u_short *)icp, cc);
-
-	if (options & F_HDRINCL) {
-		cc += sizeof(struct ip);
-		ip = (struct ip *)outpackhdr;
-		ip->ip_len = cc;
-		ip->ip_sum = in_cksum((u_short *)outpackhdr, cc);
-		packet = outpackhdr;
-	}
-	if (how_traffic_class > 1 && traffic_class >= 0) {
-		struct msghdr msg;
-		struct iovec iov;
-		char *cmbuf[CMSG_SPACE(sizeof(int))];
-		struct cmsghdr *cm = (struct cmsghdr *)cmbuf;
-
-		msg.msg_name = &whereto;
-		msg.msg_namelen = sizeof(whereto);
-
-		iov.iov_base = packet;
-		iov.iov_len = cc;
-		msg.msg_iov = &iov;
-		msg.msg_iovlen = 1;
-
-		cm->cmsg_len = CMSG_LEN(sizeof(int));
-		cm->cmsg_level = SOL_SOCKET;
-		cm->cmsg_type = SO_TRAFFIC_CLASS;
-		*(int *)CMSG_DATA(cm) = traffic_class;
-		msg.msg_control = cm;
-		msg.msg_controllen = CMSG_SPACE(sizeof(int));
-
-		msg.msg_flags = 0;
-
-		i = sendmsg(s, &msg, 0);
-	} else {
-		i = sendto(s, (char *)packet, cc, 0, (struct sockaddr *)&whereto,
-			sizeof(whereto));
-	}
-	if (i < 0 || i != cc)  {
-		if (i < 0) {
-			if (options & F_FLOOD && errno == ENOBUFS) {
-				usleep(FLOOD_BACKOFF);
-				return;
-			}
-			warn("sendto");
-		} else {
-			warn("%s: partial write: %d of %d bytes",
-			     hostname, i, cc);
-		}
-	}
+    // send echo request
+    sendto(s, (char *)packet, cc, 0, (struct sockaddr *)&whereto,
+               sizeof(whereto));
 	ntransmitted++;
 	sntransmitted++;
-	if (!(options & F_QUIET) && options & F_FLOOD)
-		(void)write(STDOUT_FILENO, &DOT, 1);
 }
-
-/*
- * pr_pack --
- *	Print out the packet, if it came from us.  This logic is necessary
- * because ALL readers of the ICMP socket get a copy of ALL ICMP packets
- * which arrive ('tis only fair).  This permits multiple copies of this
- * program to be run without having intermingled output (or statistics!).
- */
 static void
 pr_pack(char *buf, int cc, struct sockaddr_in *from, struct timeval *tv,
     int tc)
 {
-	struct in_addr ina;
 	u_char *cp, *dp;
 	struct icmp *icp;
 	struct ip *ip;
 	const void *tp;
 	double triptime;
-	int dupflag, hlen, i, j, recv_len, seq;
-	static int old_rrlen;
-	static char old_rr[MAX_IPOPTLEN];
+	int dupflag, hlen, i, recv_len, seq;
 
 	/* Check the IP header */
 	ip = (struct ip *)buf;
@@ -507,7 +320,7 @@ pr_pack(char *buf, int cc, struct sockaddr_in *from, struct timeval *tv,
 
 		if (options & F_QUIET)
 			return;
-	
+
 		if (options & F_WAITTIME && triptime > waittime) {
 			++nrcvtimeout;
 			return;
@@ -516,8 +329,7 @@ pr_pack(char *buf, int cc, struct sockaddr_in *from, struct timeval *tv,
 		if (options & F_FLOOD)
 			(void)write(STDOUT_FILENO, &BSPACE, 1);
 		else {
-			(void)printf("%d bytes from %s: icmp_seq=%u", cc,
-			   inet_ntoa(*(struct in_addr *)&from->sin_addr.s_addr),
+			(void)printf("%d bytes from %s: icmp_seq=%u", cc,inet_ntoa(*(struct in_addr *)&from->sin_addr.s_addr),
 			   seq);
 			(void)printf(" ttl=%d", ip->ip_ttl);
 			if (timing)
@@ -581,9 +393,7 @@ pr_pack(char *buf, int cc, struct sockaddr_in *from, struct timeval *tv,
  * in_cksum --
  *	Checksum routine for Internet Protocol family headers (C Version)
  */
-u_short
-in_cksum(u_short *addr, int len)
-{
+u_short in_cksum(u_short *addr, int len){
 	int nleft, sum;
 	u_short *w;
 	union {
